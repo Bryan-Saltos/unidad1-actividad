96307d1972fce79f1a772f3c6689e9b951f4bb24
Bryan-Saltos <bsaltos9905@utm.edu.ec>
2024-05-10 17:17:32 -0500

    Primeros archivos

---
+++ codigo.js
@@ -0,0 +1,7 @@
+alert("Despues de ver el contenido llene el formulario al pie de página")
+
+function mostrarDatos() {
+    var nombre = document.getElementById('nombre').value;
+    var edad = document.getElementById('edad').value;
+    alert('Nombre: ' + nombre + '\nEdad: ' + edad);
+}
